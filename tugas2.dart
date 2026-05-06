8588627cbc5024ae1f07204bb02d01faf65e47f3
hernando_ferry <hernando.ferry@gmail.com>
2026-05-06 11:08:01 +0700

    commit tugas 2 dart fundamental

---
+++ tugas2.dart
@@ -0,0 +1,27 @@
+// Tugas 2 fundamental dart //
+// Rabu 6 Mei 2026 //
+// Peserta : Ferry //
+
+void main() {
+  int nilaiUts = 100;
+  int nilaiUas = 90;
+  double kehadiran = 100;
+
+  double nilaiRataRata = (nilaiUts + nilaiUas) / 2;
+
+  if (nilaiRataRata >= 70 &&
+      kehadiran >= 75 &&
+      nilaiUas > 60 &&
+      nilaiUts > 60) {
+    print("Nilai UAS : $nilaiUas");
+    print("Nilai UTS: $nilaiUts");
+    print("Nila Rata-rata: $nilaiRataRata");
+    print("Kehadiran : $kehadiran %");
+    print("Status : Anda dinyatakan LULUS");
+  } else {
+    print("Nilai UAS : $nilaiUas");
+    print("Nilai UTS: $nilaiUts");
+    print("Nila Rata-rata: $nilaiRataRata");
+    print("Maaf silahkan mengulang di semester berikutnya");
+  }
+}
